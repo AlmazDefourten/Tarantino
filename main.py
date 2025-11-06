f82acc26ad7cfd2a4b47f778fd0ad82d4f68219a
Almaz Sakhabiev <a.sakhabiev@bars.group>
2025-11-06 08:07:39 +0300

    change model

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ print("prompt: " + prompt)
 response = requests.post(
     "http://localhost:11434/api/chat",
     json={
-        "model": "gpt-oss:20b",
+        "model": "qwen3-vl:8b",
         "messages": [
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt}
